55ae5a5051dcb3d6db137c7e36fd2eb09178ef22
Gabriel Kassarp <gkassarp@gmail.com>
2025-03-31 16:17:18 +0200

    setup ngrok

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+const ngrok = require('@ngrok/ngrok');
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const token = process.env.TELEGRAM_KEY;
-const bot = new TelegramBot(token, { polling: true });
+app.use(express.json());
 
-app.get('/test', (req, res) => {
-    bot.sendMessage('Bot is running!');
-    res.send('Message sent!');
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token, { webHook: true });
+
+
+app.post(`/bot${token}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
+
+
+const PORT = 8085;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
+
+ngrok.connect({ addr: PORT, authtoken_from_env: true })
+  .then(async (listener) => {
+    console.log(`Ingress established at: ${listener.url()}`);
+
+    const telegramWebhookUrl = `https://api.telegram.org/bot${token}/setWebhook?url=${listener.url()}`;
+
+  });
